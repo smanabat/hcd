351ed4b5fc29cfa4955140d54cc6e3aacb7153cc
sheldon <sheldon.manabat@gmail.com>
2017-02-27 21:48:38 +0000

    added sass

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sass = require('node-sass');
+var sassMiddleware = require('node-sass-middleware');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,13 +17,26 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+   
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+ sassMiddleware({
+     src: __dirname + '/sass', 
+     dest: __dirname + '/public/stylesheets',
+     prefix:  '/stylesheets'
+ })
+);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
 app.use('/users', users);
